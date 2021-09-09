8bbc0f76edad5b9f53ba4a0e9edab48f203a4178
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-09-09 16:43:30 +0530

    Added template tag for day

--- renderMonth.js
+++ renderMonth.js
@@ -7,14 +7,11 @@ import {
 } from "date-fns";
 
 export default function renderMonth(monthDate) {
-  getCalendarDates(monthDate);
+  const dates = getCalendarDates(monthDate);
 }
 
 function getCalendarDates(date) {
   const firstWeekStart = startOfWeek(startOfMonth(date), { weekStartsOn: 1 });
   const endWeekStart = endOfWeek(endOfMonth(date), { weekStartsOn: 1 });
-  const dates = eachDayOfInterval({ start: firstWeekStart, end: endWeekStart });
-  console.log(firstWeekStart);
-  console.log(endWeekStart);
-  console.log(dates);
+  return eachDayOfInterval({ start: firstWeekStart, end: endWeekStart });
 }
