e82588157806f0d6967e9fb54c372678712f809d
Jaspal Singh <jaspalsinghpersonal9@gmail.com>
2021-09-09 16:38:34 +0530

    Setup for getting each day of month

--- renderMonth.js
+++ renderMonth.js
@@ -1,10 +1,20 @@
-import { startOfMonth } from "date-fns";
+import {
+  startOfMonth,
+  startOfWeek,
+  endOfMonth,
+  endOfWeek,
+  eachDayOfInterval,
+} from "date-fns";
 
-export function renderMonth(monthDate) {
+export default function renderMonth(monthDate) {
   getCalendarDates(monthDate);
 }
 
 function getCalendarDates(date) {
-  const firstWeekStart = startOfMonth(date);
+  const firstWeekStart = startOfWeek(startOfMonth(date), { weekStartsOn: 1 });
+  const endWeekStart = endOfWeek(endOfMonth(date), { weekStartsOn: 1 });
+  const dates = eachDayOfInterval({ start: firstWeekStart, end: endWeekStart });
   console.log(firstWeekStart);
+  console.log(endWeekStart);
+  console.log(dates);
 }
